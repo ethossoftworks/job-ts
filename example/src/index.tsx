73707a53c143cc88698584ccfde2f3c035c5c4d1
Ryan Mitchener <ryanmitchener@gmail.com>
2021-01-27 10:56:56 -0500

    Updated build-system to minimize css

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -2,5 +2,6 @@ import ReactDOM from "react-dom"
 import React from "react"
 
 import { App } from "ui/app/App"
+import "static/main.scss"
 
 ReactDOM.render(<App />, document.getElementById("root"))
